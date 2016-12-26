d7871f42eb2a4370de2b2181f7a92efe18d7eb4d
Cland <dembaremba@gmail.com>
2016-12-26 19:08:24 +0200

    Installed the key packages and bootstrap related files: install-package automapper install-package bootbox install-package jquery.database install-package toastr install-package Elmah

---
+++ SmartAudit/App_Start/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace SmartAudit.App_Start 
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Mapper.Map<someclass, tosomeotherclass>();
+        }
+
+    } //end class
+} //end namespace
