79bc7f7689d74dccba29d6424537da7f745df43a
Tagumi <tagumi.solutions@gmail.com>
2016-12-26 17:46:14 +0200

    Add project files.

---
+++ SmartAudit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartAudit.Startup))]
+namespace SmartAudit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
